e0302bbb17f1862154ed47a474188003f3feac14
Bill Pate <wtpate69@hotmail.com>
2021-04-23 18:49:28 -0400

    create html for cards in javascript

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,14 +1,61 @@
-/*
+
 document.addEventListener('DOMContentLoaded', function() {
     var elems = document.querySelectorAll('.sidenav');
-    console.log("elems");
-    console.log(elems);
-    var instances = M.Sidenav.init(elems, options);
-    console.log("instances");
-    console.log(instances);
+    var instances = M.Sidenav.init(elems);
+    var projList = document.querySelector("#proj-list");  // ul
+    console.log(cardHtml(0));
+    projList.append(cardHtml(0));   
   });
+
+  var projNames = [
+      "01-Horiseon-Refactor"
+  ]
+
+cardHtml = function (i) {
+    var html;
+    html = `<div class="col s3 l3">
+    <div class="card">
+      <div class="card-image">
+        <img src="./assets/images/${projNames[i]}.png">
+      </div>
+      <div class="card-content">
+        <span class="card-title">${projNames[i]}</span>
+        <p>Refactor a website for social media and SEO company.</p>
+      </div>
+      <div class="card-action">
+        <a href="https://github.com/minprocess/${projNames[i]}">Code</a>
+        <a href="https://minprocess.github.io/${projNames[i]}">Page</a>
+      </div>
+    </div>
+  </div>
+</div>
+`
+    return html;
+}
+/*
+  <!-- 01-Horiseon Refactor -->
+  <div class="col s3 l3">
+    <div class="card">
+      <div class="card-image">
+        <img src="./assets/images/proj-1-Horiseon.jpg">
+      </div>
+      <div class="card-content">
+        <span class="card-title">Horiseon Refactor</span>
+        <p>Refactor a website for social media and SEO company.</p>
+      </div>
+      <div class="card-action">
+        <a href="https://github.com/minprocess/01-Horiseon-Refactor">Code</a>
+        <a href="https://minprocess.github.io/01-Horiseon-Refactor/">Page</a>
+      </div>
+    </div>
+  </div>
+</div>
 */
+
+
+/*
   // Or with jQuery
   $(document).ready(function(){
     $('.sidenav').sidenav();
   });
+*/
